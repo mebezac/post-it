0521e41130e814ee05ccdd68aa73ea60aafb91c9
Zachariah Clay <zacclay@gmail.com>
2014-02-18 16:05:43 -0500

    Added roles and timezone

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_user, except: [:index, :show]
+  before_action :require_admin, only: [:new, :create]
   def index
     @categories = Category.all
   end
